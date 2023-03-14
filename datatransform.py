cc0de30b63e994f3693e275a32b321f788f9eab1
ERYAGNIK003 <bhavsaryagnik003@gmail.com>
2023-03-14 23:25:23 +0530

    Update datatransform.py

--- datatransform.py
+++ datatransform.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from PIL import Image
 import csv
 
+dataset_dir=["VisDrone2019-DET-train","VisDrone2019-DET-val","VisDrone2019-DET-test-dev"]
+wd = os.getcwd()
 
 def convert(size, box):
     dw = 1. / size[0]
@@ -12,33 +14,34 @@ def convert(size, box):
     w = box[2] * dw
     h = box[3] * dh
     return (x, y, w, h)
-            
-wd = os.getcwd()
 
-if not os.path.exists('labels'):
-    os.makedirs('labels')
-
-train_file = 'images.txt'
-train_file_txt = ''
+for datadir in dataset_dir:
+    lbls=os.path.join(wd,datadir,'labels')
+    if not os.path.exists(lbls):
+        os.makedirs(lbls)
     
-anns = os.listdir('annotations')
-for ann in anns:
-    ans = ''
-    outpath = wd + '/labels/' + ann
-    if ann[-3:] != 'txt':
-        continue
-    with Image.open(wd + '/images/' + ann[:-3] + 'jpg') as Img:
-        img_size = Img.size
-    with open(wd + '/annotations/' + ann, newline='') as csvfile:
-        spamreader = csv.reader(csvfile)
-        for row in spamreader:
-            if row[4] == '0':
-                continue
-            bb = convert(img_size, tuple(map(int, row[:4])))
-            ans = ans + str(int(row[5])-1) + ' ' + ' '.join(str(a) for a in bb) + '\n'
-            with open(outpath, 'w') as outfile:
-                outfile.write(ans)
-    train_file_txt = train_file_txt + wd + '/images/' + ann[:-3] + 'jpg\n'
+    train_file = os.path.join(wd,datadir,'images.txt')
+    train_file_txt = ''
+        
+    anns = os.listdir(os.path.join(wd,datadir,'annotations'))
+    for ann in anns:
+        ans = ''
+        outpath = wd +'/'+datadir+'/'+ '/labels/' + ann
+        if ann[-3:] != 'txt':
+            continue
+        with Image.open(wd +'/'+datadir+'/'+ '/images/' + ann[:-3] + 'jpg') as Img:
+            img_size = Img.size
+        with open(wd + '/'+datadir+'/'+'/annotations/' + ann, newline='') as csvfile:
+            spamreader = csv.reader(csvfile)
+            for row in spamreader:
+                if row[4] == '0':
+                    continue
+                bb = convert(img_size, tuple(map(int, row[:4])))
+                ans = ans + str(int(row[5])-1) + ' ' + ' '.join(str(a) for a in bb) + '\n'
+                with open(outpath, 'w') as outfile:
+                    outfile.write(ans)
+        train_file_txt = train_file_txt + wd + '/'+datadir+'/'+'/images/' + ann[:-3] + 'jpg\n'
 
-with open(train_file, 'w') as outfile:
-    outfile.write(train_file_txt)
+    with open(train_file, 'w') as outfile:
+        outfile.write(train_file_txt)
+
